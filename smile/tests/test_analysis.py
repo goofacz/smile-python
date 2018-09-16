1f94026bfd909e228ec962c7a878dbf0b28a9e51
Tomasz Jankowski <tomasz.jankowski.mail@gmail.com>
2018-09-16 21:07:24 +0200

    Analysis now uses Pandas

--- smile/tests/test_analysis.py
+++ smile/tests/test_analysis.py
@@ -19,133 +19,83 @@ import matplotlib
 matplotlib.use('AGG')
 
 import unittest
-from smile.analysis import *
-import numpy as np
+import numpy.testing as npt
+from smile.result import Result
 import smile.results as sresults
+import smile.analysis as sanalysis
 
 
 class TestAnalysis(unittest.TestCase):
-    def setUp(self):
-        self.result1 = Result()
-        self.result1.mac_address = 112233445566
-        self.result1.position_dimensions = 2
-        self.result1.position_x = 1
-        self.result1.position_y = 2
-        self.result1.position_z = 3
-        self.result1.begin_true_position_x = 4
-        self.result1.begin_true_position_y = 5
-        self.result1.begin_true_position_z = 6
-        self.result1.end_true_position_x = 7
-        self.result1.end_true_position_y = 8
-        self.result1.end_true_position_z = 9
+    def test_compute_basic_statistics(self):
+        result1 = Result()
+        result1.mac_address = 112233445566
+        result1.position_dimensions = 2
+        result1.position_x = 1
+        result1.position_y = 2
+        result1.position_z = 3
+        result1.begin_true_position_x = 4
+        result1.begin_true_position_y = 5
+        result1.begin_true_position_z = 6
+        result1.end_true_position_x = 5
+        result1.end_true_position_y = 6
+        result1.end_true_position_z = 7
+        result1.reference_position_x = 4.5
+        result1.reference_position_y = 5.5
+        result1.reference_position_z = 6.5
 
-        self.result2 = Result()
-        self.result2.mac_address = 212233445566
-        self.result2.position_dimensions = 2
-        self.result2.position_x = 10
-        self.result2.position_y = 20
-        self.result2.position_z = 30
-        self.result2.begin_true_position_x = 40
-        self.result2.begin_true_position_y = 50
-        self.result2.begin_true_position_z = 60
-        self.result2.end_true_position_x = 70
-        self.result2.end_true_position_y = 80
-        self.result2.end_true_position_z = 90
+        result2 = Result()
+        result2.mac_address = 112233445566
+        result2.position_dimensions = 2
+        result2.position_x = 2
+        result2.position_y = 4
+        result2.position_z = 6
+        result2.begin_true_position_x = 4
+        result2.begin_true_position_y = 5
+        result2.begin_true_position_z = 6
+        result2.end_true_position_x = 5
+        result2.end_true_position_y = 6
+        result2.end_true_position_z = 7
+        result2.reference_position_x = 4.5
+        result2.reference_position_y = 5.5
+        result2.reference_position_z = 6.5
 
-        self.result3 = Result()
-        self.result3.mac_address = 312233445566
-        self.result3.position_dimensions = 2
-        self.result3.position_x = 100
-        self.result3.position_y = 200
-        self.result3.position_z = 300
-        self.result3.begin_true_position_x = 400
-        self.result3.begin_true_position_y = 500
-        self.result3.begin_true_position_z = 600
-        self.result3.end_true_position_x = 700
-        self.result3.end_true_position_y = 800
-        self.result3.end_true_position_z = 900
+        results = sresults.create_results([result1, result2])
+        statistics = sanalysis.compute_basic_statistics(results)
 
-        self.result4 = Result()
-        self.result4.mac_address = 112233445566
-        self.result4.position_dimensions = 2
-        self.result4.position_x = 2
-        self.result4.position_y = 4
-        self.result4.position_z = 6
-        self.result4.begin_true_position_x = 8
-        self.result4.begin_true_position_y = 10
-        self.result4.begin_true_position_z = 12
-        self.result4.end_true_position_x = 14
-        self.result4.end_true_position_y = 16
-        self.result4.end_true_position_z = 18
+        self.assertEqual(112233445566, statistics['mac_address'][0])
+        self.assertEqual(4.5, statistics['reference_position_x'][0])
+        self.assertEqual(5.5, statistics['reference_position_y'][0])
+        self.assertEqual(6.5, statistics['reference_position_z'][0])
 
-        self.result5 = Result()
-        self.result5.mac_address = 212233445566
-        self.result5.position_dimensions = 2
-        self.result5.position_x = 15
-        self.result5.position_y = 25
-        self.result5.position_z = 35
-        self.result5.begin_true_position_x = 45
-        self.result5.begin_true_position_y = 55
-        self.result5.begin_true_position_z = 65
-        self.result5.end_true_position_x = 75
-        self.result5.end_true_position_y = 85
-        self.result5.end_true_position_z = 95
+        # positions
+        self.assertEqual(2, statistics['positions']['count'][0])
 
-        self.reference_results = [self.result1, self.result2, self.result3, self.result4, self.result5]
+        # position_x
+        self.assertEqual(1, statistics['position_x']['min'][0])
+        self.assertEqual(2, statistics['position_x']['max'][0])
+        npt.assert_array_almost_equal(0.707107, statistics['position_x']['std'][0])
+        self.assertEqual(0.5, statistics['position_x']['var'][0])
+        self.assertEqual(1.25, statistics['position_x']['25%'][0])
+        self.assertEqual(1.5, statistics['position_x']['50%'][0])
+        self.assertEqual(1.75, statistics['position_x']['75%'][0])
 
-    def test_squeeze_results_mean(self):
-        results = sresults.create_results(self.reference_results)
+        # position_y
+        self.assertEqual(2, statistics['position_y']['min'][0])
+        self.assertEqual(4, statistics['position_y']['max'][0])
+        npt.assert_array_almost_equal(1.414214, statistics['position_y']['std'][0])
+        self.assertEqual(2, statistics['position_y']['var'][0])
+        self.assertEqual(2.5, statistics['position_y']['25%'][0])
+        self.assertEqual(3, statistics['position_y']['50%'][0])
+        self.assertEqual(3.5, statistics['position_y']['75%'][0])
 
-        unique_results = squeeze_results(results, grouping_method="mac_address", averaging_method="mean")
-        self.assertTupleEqual((3, 14), unique_results.shape)
-        self.assertEqual(112233445566, unique_results.loc[0, "mac_address"])
-        np.testing.assert_equal((1.5, 3), unique_results.loc[0, ["position_x", 'position_y']].values)
-        np.testing.assert_equal((6, 7.5), unique_results.loc[0, ["begin_true_position_x", "begin_true_position_y"]].values)
-        np.testing.assert_equal((10.5, 12), unique_results.loc[0, ["end_true_position_x", "end_true_position_y"]].values)
-        self.assertEqual(212233445566, unique_results.loc[1, "mac_address"])
-        np.testing.assert_equal((12.5, 22.5), unique_results.loc[1, ["position_x", 'position_y']].values)
-        np.testing.assert_equal((42.5, 52.5), unique_results.loc[1, ["begin_true_position_x", "begin_true_position_y"]].values)
-        np.testing.assert_equal((72.5, 82.5), unique_results.loc[1, ["end_true_position_x", "end_true_position_y"]].values)
-        self.assertEqual(312233445566, unique_results.loc[2, "mac_address"])
-        np.testing.assert_equal((100, 200), unique_results.loc[2, ["position_x", 'position_y']].values)
-        np.testing.assert_equal((400, 500), unique_results.loc[2, ["begin_true_position_x", "begin_true_position_y"]].values)
-        np.testing.assert_equal((700, 800), unique_results.loc[2, ["end_true_position_x", "end_true_position_y"]].values)
-
-    def test_squeeze_results_max(self):
-        results = sresults.create_results(self.reference_results)
-
-        unique_results = squeeze_results(results, grouping_method="mac_address", averaging_method="max")
-        self.assertTupleEqual((3, 14), unique_results.shape)
-        self.assertEqual(112233445566, unique_results.loc[0, "mac_address"])
-        np.testing.assert_equal((2, 4), unique_results.loc[0, ["position_x", 'position_y']].values)
-        np.testing.assert_equal((8, 10), unique_results.loc[0, ["begin_true_position_x", "begin_true_position_y"]].values)
-        np.testing.assert_equal((14, 16), unique_results.loc[0, ["end_true_position_x", "end_true_position_y"]].values)
-        self.assertEqual(212233445566, unique_results.loc[1, "mac_address"])
-        np.testing.assert_equal((15, 25), unique_results.loc[1, ["position_x", 'position_y']].values)
-        np.testing.assert_equal((45, 55), unique_results.loc[1, ["begin_true_position_x", "begin_true_position_y"]].values)
-        np.testing.assert_equal((75, 85), unique_results.loc[1, ["end_true_position_x", "end_true_position_y"]].values)
-        self.assertEqual(312233445566, unique_results.loc[2, "mac_address"])
-        np.testing.assert_equal((100, 200), unique_results.loc[2, ["position_x", 'position_y']].values)
-        np.testing.assert_equal((400, 500), unique_results.loc[2, ["begin_true_position_x", "begin_true_position_y"]].values)
-        np.testing.assert_equal((700, 800), unique_results.loc[2, ["end_true_position_x", "end_true_position_y"]].values)
-
-    def test_squeeze_results_min(self):
-        results = sresults.create_results(self.reference_results)
-
-        unique_results = squeeze_results(results, grouping_method="mac_address", averaging_method="min")
-        self.assertTupleEqual((3, 14), unique_results.shape)
-        self.assertEqual(112233445566, unique_results.loc[0, "mac_address"])
-        np.testing.assert_equal((1, 2), unique_results.loc[0, ["position_x", 'position_y']].values)
-        np.testing.assert_equal((4, 5), unique_results.loc[0, ["begin_true_position_x", "begin_true_position_y"]].values)
-        np.testing.assert_equal((7, 8), unique_results.loc[0, ["end_true_position_x", "end_true_position_y"]].values)
-        self.assertEqual(212233445566, unique_results.loc[1, "mac_address"])
-        np.testing.assert_equal((10, 20), unique_results.loc[1, ["position_x", 'position_y']].values)
-        np.testing.assert_equal((40, 50), unique_results.loc[1, ["begin_true_position_x", "begin_true_position_y"]].values)
-        np.testing.assert_equal((70, 80), unique_results.loc[1, ["end_true_position_x", "end_true_position_y"]].values)
-        self.assertEqual(312233445566, unique_results.loc[2, "mac_address"])
-        np.testing.assert_equal((100, 200), unique_results.loc[2, ["position_x", 'position_y']].values)
-        np.testing.assert_equal((400, 500), unique_results.loc[2, ["begin_true_position_x", "begin_true_position_y"]].values)
-        np.testing.assert_equal((700, 800), unique_results.loc[2, ["end_true_position_x", "end_true_position_y"]].values)
+        # position_y
+        self.assertEqual(3, statistics['position_z']['min'][0])
+        self.assertEqual(6, statistics['position_z']['max'][0])
+        npt.assert_array_almost_equal(2.12132, statistics['position_z']['std'][0])
+        self.assertEqual(4.5, statistics['position_z']['var'][0])
+        self.assertEqual(3.75, statistics['position_z']['25%'][0])
+        self.assertEqual(4.5, statistics['position_z']['50%'][0])
+        self.assertEqual(5.25, statistics['position_z']['75%'][0])
 
 
 if __name__ == '__main__':
